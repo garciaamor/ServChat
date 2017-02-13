80b88bc671086c5942c0a795fd0271b63c32ed99
garciaamor <jgarciaamor@danielcastelao.org>
2017-02-13 10:59:50 +0100

    Servidor Chat

--- src/servchat/ServChat.java
+++ src/servchat/ServChat.java
@@ -5,11 +5,13 @@ package servchat;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.ServerSocket;
@@ -17,6 +19,7 @@ import javax.swing.JOptionPane;
 
 public class ServChat extends Thread{
 
+        public static String textC;
         public static String text;
         public static String cadComprob="cerrar";
         static int puerto=5555;
@@ -27,39 +30,27 @@ public class ServChat extends Thread{
        
             ServerSocket serverSocket=new ServerSocket();
             
-            
             InetSocketAddress addr=new InetSocketAddress("localhost",puerto);
             serverSocket.bind(addr);
 
             System.out.println("Aceptando conexiones");
-            
-                        
+                     
             Socket newSocket= serverSocket.accept();
-                                    
+                                   
             System.out.println("Conexion recibida");
             
+            DataInputStream is = new DataInputStream(newSocket.getInputStream());
+            PrintStream os = new PrintStream(newSocket.getOutputStream());
             
-
-            BufferedReader is = new BufferedReader(new InputStreamReader(newSocket.getInputStream()));
-            
-            
-            BufferedWriter os = new BufferedWriter(new OutputStreamWriter(newSocket.getOutputStream()));
            
-            text=is.readLine();
-            System.out.println("Mensaje que envia el cliente :"+ text);
-            
+            textC=is.readLine();
+            System.out.println("Mensaje que envia el cliente :"+ textC);
             text = JOptionPane.showInputDialog("(S)Escribe un mensaje");
-            os.write(text);
             
-            if (text.equals(cadComprob)){
-                numComp=5;
-            }
+             
             
-            if (numComp==5){
-                System.out.println("LOS TEXTOS COINCIDEN");
-            newSocket.close();     
+            
             serverSocket.close();
             }
        
     }
-}
